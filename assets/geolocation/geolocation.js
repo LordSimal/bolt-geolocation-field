54756704d1357d8bbb783517e6a62026d6c178a6
Kevin Pfeifer <kevin.pfeifer@sunlime.at>
2020-10-02 23:56:54 +0200

    added possibility to zoom in and out and save that zoom state

--- assets/geolocation/geolocation.js
+++ assets/geolocation/geolocation.js
@@ -1,8 +1,20 @@
 ( function( $ ) {
 
   let geolocation_wrapper = $( '.geolocation-field-wrapper' );
+  let basic_gmap_url = "https://maps.google.com/maps?q=$QUERY$&t=&z=$ZOOM$&ie=UTF8&iwloc=&output=embed"
 
   if( geolocation_wrapper.length ) {
+
+    function get_hidden_input_val($hidden_input){
+      let db_val = $hidden_input.val();
+      if(db_val === ""){
+        db_val = {};
+      } else {
+        db_val = JSON.parse(db_val);
+      }
+      return db_val;
+    }
+
     geolocation_wrapper.each( function( idx, elem ) {
       let $wrapper = $( elem );
 
@@ -21,31 +33,23 @@
       // Toggle inputs depending on which radio is active
       $search_radio.add($latlong_radio).on('change', function(e){
         let $this = $(this);
-        let db_val = $hidden_input.val();
-        if(db_val === ""){
-          db_val = {};
-        } else {
-          db_val = JSON.parse(db_val);
-        }
+        let hidden_input_val = get_hidden_input_val($hidden_input);
 
-        let $is_search = false;
         if($this.parent().hasClass('geolocation-radio-search')){
-          $is_search = true;
-        }
-
-        if($is_search){
           $search_input.parent().removeClass('d-none');
           $lat_input.parent().addClass('d-none');
           $long_input.parent().addClass('d-none');
-          db_val.selected = 'search';
+          hidden_input_val.selected = 'search';
         } else {
           $search_input.parent().addClass('d-none');
           $lat_input.parent().removeClass('d-none');
           $long_input.parent().removeClass('d-none');
-          db_val.selected = 'latlong';
+          hidden_input_val.selected = 'latlong';
         }
 
-        $hidden_input.val(JSON.stringify(db_val));
+        hidden_input_val.zoom = $hidden_zoom_input.val();
+
+        $hidden_input.val(JSON.stringify(hidden_input_val));
 
       });
 
@@ -55,25 +59,75 @@
         let $parent = $this.parent();
         let value = $this.val();
 
-        let db_val = $hidden_input.val();
-        if(db_val === ""){
-          db_val = {};
-        } else {
-          db_val = JSON.parse(db_val);
-        }
+        let hidden_input_val = get_hidden_input_val($hidden_input);
 
         if($parent.hasClass('geolocation-field-search')){
-          db_val.search = value;
+          hidden_input_val.search = value;
         } else if($parent.hasClass('geolocation-field-lat')) {
-          db_val.lat = value;
+          hidden_input_val.lat = value;
         } else if($parent.hasClass('geolocation-field-long')) {
-          db_val.long = value;
+          hidden_input_val.long = value;
         }
 
-        $hidden_input.val(JSON.stringify(db_val));
+        hidden_input_val.zoom = $hidden_zoom_input.val();
+
+        $hidden_input.val(JSON.stringify(hidden_input_val));
 
       });
 
+      // ========== GMAP ==========
+
+      // Update GMap Button
+      let $update_gmap_btn = $wrapper.find('.js-update-gmap');
+
+      // Update GMap Preview when clicking on button
+      $update_gmap_btn.on('click', function(e){
+        let gmap_url = "";
+        if($search_radio.is(':checked')){
+          let search_val = $search_input.val();
+          gmap_url = basic_gmap_url.replace('$QUERY$', search_val);
+        } else {
+          let lat = $lat_input.val();
+          let long = $long_input.val();
+          let query = lat + ',' + long;
+          gmap_url = basic_gmap_url.replace('$QUERY$', query);
+        }
+
+        let zoom = $('.js-geolocation-zoom').val();
+        gmap_url = gmap_url.replace('$ZOOM$', zoom);
+        $wrapper.find('.js-gmap').attr('src', encodeURI(gmap_url));
+
+        // Update hidden input
+        let hidden_input_val = get_hidden_input_val($hidden_input);
+        hidden_input_val.zoom = zoom;
+
+        $hidden_input.val(JSON.stringify(hidden_input_val));
+
+      });
+
+      // Zoom GMap Buttons
+      let $zoom_gmap_in = $wrapper.find('.js-zoom-gmap-in');
+      let $zoom_gmap_out = $wrapper.find('.js-zoom-gmap-out');
+      let $hidden_zoom_input = $wrapper.find('.js-geolocation-zoom');
+
+      // Zoom GMap Preview when clicking on buttons
+      $zoom_gmap_in.add($zoom_gmap_out).on('click', function(e){
+        let current_zoom = parseInt($hidden_zoom_input.val());
+        let new_zoom = current_zoom;
+
+        if($(this).hasClass('js-zoom-gmap-in')){
+          if(current_zoom < 19){
+            new_zoom = current_zoom + 1;
+          }
+        } else if($(this).hasClass('js-zoom-gmap-out')) {
+          if(current_zoom > 0){
+            new_zoom = current_zoom - 1;
+          }
+        }
+
+        $hidden_zoom_input.val(new_zoom);
+        $update_gmap_btn.trigger('click');
+      });
 
     } );
 
